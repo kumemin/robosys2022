f153c4db97dbac616f9030037f0a33455f2f80eb
Kumemin <s21c1042ll@s.chibakoudai.jp>
2022-11-29 11:01:41 +0900

    add

--- test.bash
+++ test.bash
@@ -10,8 +10,8 @@ ng () {
 res=0
 
 ### I/O TEST ###
-out=$(seq 5 | ./plus)
-[ "${out}" = "15 0b1111 0xf" ] || ng ${LINENO}
+out=$(seq 5 | ./plus) #
+[ "${out}" = "15 0b1111 0xf" ] || ng ${LINENO} #seq 5 | ./plusで"15 0b1111 0xf"が出れば成功
 
 ### STRANGE INPUT ###
 out=$(echo あ | ./plus)
